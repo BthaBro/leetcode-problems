ada19c0dcce52de3fb3cf7d818af313651ac6342
Abror Khamrokhujaev <abrorkhodza@gmail.com>
2021-06-27 13:00:56 +0500

    max-consecutive-ones added

---
+++ easy/max-consecutive-ones.go
@@ -0,0 +1,21 @@
+// //https://leetcode.com/problems/max-consecutive-ones/
+// //Date 27.06.21
+package easy
+
+func findMaxConsecutiveOnes(nums []int) int {
+	var result, count int
+
+	for _, num := range nums {
+		if num == 1 {
+			count++
+		} else {
+			count = 0
+		}
+
+		if count > result {
+			result = count
+		}
+	}
+
+	return result
+}
